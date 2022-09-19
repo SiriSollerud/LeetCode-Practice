1ca6c6311545cf1e75d57bc26b1b05a6ad4cda52
Siri <35779435+SiriSollerud@users.noreply.github.com>
2022-09-19 11:35:19 +0200

    Time: 1297 ms (23.90%), Space: 16.1 MB (10.89%) - LeetHub

--- 125-valid-palindrome/125-valid-palindrome.py
+++ 125-valid-palindrome/125-valid-palindrome.py
@@ -1,37 +1,23 @@
 class Solution(object):
     def isPalindrome(self, s):
-        
-        # return true if palindrome
-        #   convert to lowercase
-        #   remove all non-alphanumeric chars
-        #   alpha-numeric = letters and numbers
 
         if len(s) == 1:
             return True
         
-        s = s.replace(" ", "")
-        s = s.lower()
+        s = s.lower().replace(" ", "")
+        
         for letter in s:
             if not letter.isalnum():
                 s = s.replace(letter, "")
+        
         if len(s) == 0:
             return True
 
-        check = False
         count_unique = 0
         for i in range(len(s)):
-            if s[i] == s[-i - 1]:
-                check = True
-            else:
+            if not s[i] == s[-i - 1]:
                 count_unique += 1
-                check = False
-                
-        if check and count_unique <= 1:
-            return True
-        else:
-            return False
-        """
-        :type s: str
-        :rtype: bool
-        """
+                if count_unique > 1:
+                    return False
+        return True
         
